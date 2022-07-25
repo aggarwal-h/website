4b6ab54c0d761cd25b0390abb7a4f10a4e3d8699
aggarwal-h <himanshu@preteckt.com>
2022-07-24 23:29:24 -0400

    Update LogoPill to use Nextjs Link component

--- components/logo/LogoPill.js
+++ components/logo/LogoPill.js
@@ -1,12 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function LogoPill() {
   return (
-    <a
-      className="text-white text-2xl font-poppins font-extrabold inline-block mr-2 tracking-widest bg-gradient-to-r from-blue-700 to to-indigo-400 rounded-t-3xl rounded-b-3xl py-2 px-4"
-      href=""
-    >
-      <span>Himanshu</span>
-    </a>
+    <Link href="/">
+      <a className="text-white text-2xl font-poppins font-extrabold inline-block mr-2 tracking-widest bg-gradient-to-r from-blue-700 to to-indigo-400 rounded-t-3xl rounded-b-3xl py-2 px-4">
+        <span>Himanshu</span>
+      </a>
+    </Link>
   );
 }
